2ac9d1f2fa63f0038eb0b3e53f1942bb91039e7a
sophalkim <sophal.kim.work@gmail.com>
2015-02-20 20:47:19 -0800

    Horizontal Scroll works much better than viewpager in the product description class

--- src/ssk/porject/grouponclone/ProductDescription.java
+++ src/ssk/porject/grouponclone/ProductDescription.java
@@ -2,12 +2,25 @@ package ssk.porject.grouponclone;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class ProductDescription extends Activity {
-
+	LinearLayout linearLayout;
+	ImageView imageView;
+	ImageView imageView2;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.product_description_layout);
+
+		imageView = new ImageView(this);
+		imageView2 = new ImageView(this);
+		imageView.setImageResource(R.drawable.cat_food);
+		imageView2.setImageResource(R.drawable.view_pager_background);
+		linearLayout = (LinearLayout) findViewById(R.id.gallery);
+		linearLayout.addView(imageView);
+		linearLayout.addView(imageView2);
 	}
 }
